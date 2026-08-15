2252fa15fd0a4f279ca6cd324d82b7103d2537d3
davidvelino <davidvelino.ai@gmail.com>
2026-08-15 18:03:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ APIFY_TOKEN = os.getenv("APIFY_TOKEN")
 
 # Kata kunci yang dipantau
 KEYWORDS = ["saham", "crypto", "bitcoin", "ihsg", "btc"]
-MIN_VIEWS = 10000  # Trigger 100k views
+MIN_VIEWS = 10000  # Trigger 10k views
 
 def send_telegram_alert(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
