a27b1c4224fde9ff7bb54aad27d0cf8b18bbe083
davidvelino <davidvelino.ai@gmail.com>
2026-08-16 22:31:09 +0800

    Change error message from Apify to Telegram

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def send_telegram_alert(message):
     try:
         requests.post(url, json=payload)
     except Exception as e:
-    print(f"ERROR APIFY: {e}")
+        print(f"ERROR TELEGRAM: {e}")
 
 def check_social_media():
     print("Memulai pengecekan postingan viral...")
